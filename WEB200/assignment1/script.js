79dfdadbd377ca95484686ae5bf0fd08df34c455
Corey Tranchita <c.tranchita@protonmail.ch>
2023-08-28 22:17:50 -0500

    added css to pages as well as finished asignment1

--- WEB200/assignment1/script.js
+++ WEB200/assignment1/script.js
@@ -1,1 +1,10 @@
-document.write("Hello Corey!");
+// this file is connected to index.html for assignment 1 (AKA external Javascript file). 
+//It writes to the webpage, alerts, finds the ID of a paragraph tag id'd as "happy", and logs everything to the web browser console.
+
+document.write("This is in a JS file: Document.write();");
+
+window.alert("This is in a JS file");
+
+document.getElementById("happy").innerHTML = "This is in a JS file: document.getElementById().innerHTML";
+
+console.log("This is in a JS file");
