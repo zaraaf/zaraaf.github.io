e207a9b3abfdfff83ecd10b45089cfc514f2d67e
Corey Tranchita <c.tranchita@protonmail.ch>
2023-09-11 21:49:11 -0500

    Finished assignment 5

--- WEB200/assignment5/activity5.js
+++ WEB200/assignment5/activity5.js
@@ -7,17 +7,57 @@ function sockSize() {
         document.getElementById("size").innerHTML = "PLEASE CLICK AGAIN AND CHOOSE A REAL SHOE SIZE!"
     } else {
 
-    if (sSize < 4) {
-        document.getElementById("size").innerHTML = "your sock size is extra small!";
-    } else if (sSize >= 4 && sSize <= 6) {
-        document.getElementById("size").innerHTML = "your sock size is small!";
-    } else if (sSize >= 7 && sSize <= 9) {
-        document.getElementById("size").innerHTML = "your sock size is medium!";
-    } else if (sSize >= 10 && sSize <= 12) {
-        document.getElementById("size").innerHTML = "your sock size is large!";
-    } else if (sSize >= 13) {
-        document.getElementById("size").innerHTML = "your sock size is extra large!";
+        if (sSize < 4) {
+            document.getElementById("size").innerHTML = "your sock size is extra small!";
+        } else if (sSize >= 4 && sSize <= 6) {
+            document.getElementById("size").innerHTML = "your sock size is small!";
+        } else if (sSize >= 7 && sSize <= 9) {
+            document.getElementById("size").innerHTML = "your sock size is medium!";
+        } else if (sSize >= 10 && sSize <= 12) {
+            document.getElementById("size").innerHTML = "your sock size is large!";
+        } else if (sSize >= 13) {
+            document.getElementById("size").innerHTML = "your sock size is extra large!";
+        }
     }
- }
 
+}
+
+
+
+function shoeSize() {
+
+    // function getSize() {
+    //     let sSize = window.prompt("What is your shoe size?", 1);
+    // }
+
+
+
+    let sSize = window.prompt("What is your shoe size?", 1);
+
+    switch (sSize) {
+        case "1":
+        case "2":
+        case "3":
+            document.getElementById("newSize").innerHTML = "Your shoe size is extra small";
+            break;
+        case "4":     
+        case "5":
+        case "6":
+            document.getElementById("newSize").innerHTML = "Your shoe size is small";
+            break;
+        case "7":     
+        case "8":
+        case "9":
+            document.getElementById("newSize").innerHTML = "Your shoe size is medium";
+            break;
+        case "10":     
+        case "11":
+        case "12":
+            document.getElementById("newSize").innerHTML = "Your shoe size is large";
+            break;
+        default:
+            document.getElementById("newSize").innerHTML = "Your shoe size is extra large";
+            break;
+    }
+    
 }
