6b5fa1a92bd99ff7eea0ffb36e12d0b4104a8cdd
mheliker <62227421+mheliker@users.noreply.github.com>
2023-10-16 20:10:52 -0500

    Update activity6.js
    Teacher addign random comments for assignment 6...

--- WEB200/assignment6/activity6.js
+++ WEB200/assignment6/activity6.js
@@ -12,12 +12,27 @@ function multi() {
 
 function guess() {
     let userGuess = window.prompt("Pick a number between 0 and 100");
+    let high = 100;
+    let low = 0;
 
+    // if user says 65
+    // if not equal to high or low value
+    // average of the (high + low)/2 = 50 if user Guess is higher or lower 
+    // since user guess is higher than 50
+    // we know that number between 50 and 100
+    // 150/2 = 75.... 
+    // now it's lower than 75... so 75 is the new "high"
+    // is user Guess higher or lower than 75.. it's Lower (i.e. 65)
+    // 75 + 50 = 125/2 = 63... 
+    // is userGuess higher or lower than 63... it's higher
+    // 63 is the new "low" and 75 is still the "high"
+
+    // do while loop... that compares the user's guess to the average of high and low... 
     //failsafe if the user picked a number below 0 or above 100
     if (userGuess < 0 || userGuess > 100) {
         window.alert("PLEASE PICK A NUMBER BETWEEN 0 AND 100!!");
     }
     else {
-        dowhile ()
+        do while ()
     }
 }
