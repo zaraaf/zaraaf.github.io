7462e0a7e2e3ea457d51d4a1821bb33627cb8957
Corey <Zaraaf445@gmail.com>
2023-10-24 19:47:00 -0500

    Started on assignment 9

---
+++ WEB200/assignment9/activity9.js
@@ -0,0 +1,27 @@
+function getDate() {
+   const date = new Date();
+   let year = date.getFullYear();
+   let month = date.getMonth();
+   let day = date.getDay();
+   let hours = date.getHours();
+   let minutes = date.getMinutes();
+   let seconds = date.getSeconds();
+
+   const daysOWeek = [Sunday, Monday, Tuesday, Wednesday, Thursday, Friday, Saturday];
+
+   document.getElementById("date").innerHTML = "Full date: " + date;
+   document.getElementById("year").innerHTML = "Year: " + year;
+   document.getElementById("month").innerHTML = "Months: " + month;
+   document.getElementById("days").innerHTML = "Days: " + day;
+   document.getElementById("hours").innerHTML = "Hours: " + hours;
+   document.getElementById("minutes").innerHTML = "Minutes: " + minutes;
+   document.getElementById("seconds").innerHTML = "Seconds " + seconds;
+   
+   // year = date.getFullYear;
+   // month = date.getMonth;
+   // day = date.getDay;
+   // hours = date.getHours;
+   // minutes = date.getMinutes;
+   // seconds = date.getSeconds;
+   setInterval(getDate, 1000);
+}
